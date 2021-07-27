de1e1112448db7b6fb76a32d050148492cdfbeca
Efrem Zapryagaev <efremz.dev@gmail.com>
2021-07-27 17:54:28 +0300

    3

--- src/main/java/ru/ez/simpleWebApp/GreetingController.java
+++ src/main/java/ru/ez/simpleWebApp/GreetingController.java
@@ -20,15 +20,13 @@ public class GreetingController {
         this.messageRepo = messageRepo;
     }
 
-    @GetMapping("/greeting")
-    public String greeting(
-            @RequestParam(name="name", required=false, defaultValue="World") String name,
-            Map<String, Object> model) {
-        model.put("name", name);
+    @GetMapping("/")
+    public String greeting(Map<String, Object> model) {
+
         return "greeting";
     }
 
-    @GetMapping
+    @GetMapping("/main")
     public String main(Map<String, Object> model){
         Iterable<Message> messages = messageRepo.findAll();
 
@@ -36,7 +34,7 @@ public class GreetingController {
         return "main";
     }
 
-    @PostMapping
+    @PostMapping("/main")
     public String add(@RequestParam String text, @RequestParam String tag, Map<String, Object> model){
         Message message = new Message(text, tag);
         messageRepo.save(message);
