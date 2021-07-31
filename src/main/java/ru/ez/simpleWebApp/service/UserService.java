6d36c8fb2d2bef616347b7df6ad4d403c2d20e25
Efrem Zapryagaev <efremz.dev@gmail.com>
2021-07-31 18:59:45 +0300

    10

--- src/main/java/ru/ez/simpleWebApp/service/UserService.java
+++ src/main/java/ru/ez/simpleWebApp/service/UserService.java
@@ -10,16 +10,15 @@ import ru.ez.simpleWebApp.domain.Role;
 import ru.ez.simpleWebApp.domain.User;
 import ru.ez.simpleWebApp.repo.UserRepo;
 
-import java.util.Collections;
-import java.util.UUID;
+import java.util.*;
+import java.util.stream.Collectors;
 
 @Service
 public class UserService implements UserDetailsService {
     @Autowired
-    private  UserRepo userRepo;
+    private UserRepo userRepo;
     @Autowired
     private MailSenderService mailSenderService;
-
 
 
     @Override
@@ -27,10 +26,10 @@ public class UserService implements UserDetailsService {
         return userRepo.findByUsername(username);
     }
 
-    public boolean addUser(User user){
+    public boolean addUser(User user) {
         User userFromDb = userRepo.findByUsername(user.getUsername());
 
-        if(userFromDb !=null){
+        if (userFromDb != null) {
             return false;
         }
 
@@ -40,24 +39,29 @@ public class UserService implements UserDetailsService {
 
         userRepo.save(user);
 
-        if(!StringUtils.isEmpty(user.getEmail())){
+        sendMessage(user);
+
+
+        return true;
+    }
+
+    private void sendMessage(User user) {
+        if (!StringUtils.isEmpty(user.getEmail())) {
             String message = String.format(
-              "Hello, %s! \n" +
-                      "Welcome to ezer! Visit http://localhost:8080/activate/%s",
+                    "Hello, %s! \n" +
+                            "Welcome to ezer! Visit http://localhost:8080/activate/%s",
                     user.getUsername(),
                     user.getActivationCode()
             );
 
-            mailSenderService.send(user.getEmail(),"Activation code",message);
+            mailSenderService.send(user.getEmail(), "Activation code", message);
         }
-
-        return true;
     }
 
     public boolean activateUser(String code) {
         User user = userRepo.findByActivationCode(code);
 
-        if(user == null){
+        if (user == null) {
             return false;
         }
 
@@ -66,5 +70,51 @@ public class UserService implements UserDetailsService {
         userRepo.save(user);
 
         return true;
+    }
+
+    public List<User> findAll() {
+        return userRepo.findAll();
+    }
+
+    public void saveUser(User user, String username, Map<String, String> form) {
+        user.setUsername(username);
+
+        Set<String> roles = Arrays.stream(Role.values())
+                .map(Role::name)
+                .collect(Collectors.toSet());
+
+        user.getRoles().clear();
+
+        for (String key : form.keySet()) {
+            if (roles.contains(key)) {
+                user.getRoles().add(Role.valueOf(key));
+            }
+        }
+        userRepo.save(user);
+    }
+
+    public void updateProfile(User user, String password, String email) {
+        String userEmail = user.getEmail();
+
+        boolean isEmailChanged = (email != null && !email.equals(userEmail)) ||
+                (userEmail != null && !userEmail.equals(email));
+
+        if (isEmailChanged) {
+            user.setEmail(email);
+
+            if (!StringUtils.isEmpty(email)) {
+                user.setActivationCode(UUID.randomUUID().toString());
+            }
+        }
+
+        if (!StringUtils.isEmpty(password)) {
+            user.setPassword(password);
+        }
+
+        userRepo.save(user);
+
+        if (isEmailChanged) {
+            sendMessage(user);
+        }
     }
 }
