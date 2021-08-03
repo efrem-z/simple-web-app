33698f59197f4c9185f635b7e3cc40faf24cc7d8
Efrem Zapryagaev <efremz.dev@gmail.com>
2021-08-03 16:42:29 +0300

    add login test

---
+++ src/test/java/ru/ez/simpleWebApp/LoginTest.java
@@ -0,0 +1,45 @@
+package ru.ez.simpleWebApp;
+
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import ru.ez.simpleWebApp.controller.MainController;
+
+import static org.hamcrest.Matchers.containsString;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@AutoConfigureMockMvc
+public class LoginTest {
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Autowired
+    private MainController controller;
+
+    @Test
+    public void contextLoad() throws Exception{
+        this.mockMvc.perform(get("/")).
+                andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(content().string(containsString("Hello, guest")))
+                .andExpect(content().string(containsString("Please, login")));
+    }
+
+    @Test
+    public void loginTest() throws Exception {
+        this.mockMvc.perform(get("/main"))
+                .andDo(print())
+                .andExpect(status().is3xxRedirection())
+                .andExpect(redirectedUrl("http://localhost/login"));
+    }
+}
