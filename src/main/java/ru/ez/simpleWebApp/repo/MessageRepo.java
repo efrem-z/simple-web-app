3f41fbf94ec791b6ff27fafe44beef75978aef9e
Efrem Zapryagaev <efremz.dev@gmail.com>
2021-08-03 13:38:06 +0300

    15

--- src/main/java/ru/ez/simpleWebApp/repo/MessageRepo.java
+++ src/main/java/ru/ez/simpleWebApp/repo/MessageRepo.java
@@ -5,7 +5,7 @@ import ru.ez.simpleWebApp.domain.Message;
 
 import java.util.List;
 
-public interface MessageRepo extends CrudRepository<Message, Integer> {
+public interface MessageRepo extends CrudRepository<Message, Long> {
    List<Message> findByTag(String tag);
 
 
