c414dd29d8d08cfa43849c194ba6d02058e7ac54
Efrem Zapryagaev <efremz.dev@gmail.com>
2021-08-06 13:35:23 +0300

    add pagination

--- src/main/java/ru/ez/simpleWebApp/repo/MessageRepo.java
+++ src/main/java/ru/ez/simpleWebApp/repo/MessageRepo.java
@@ -1,12 +1,13 @@
 package ru.ez.simpleWebApp.repo;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.repository.CrudRepository;
 import ru.ez.simpleWebApp.domain.Message;
 
-import java.util.List;
-
 public interface MessageRepo extends CrudRepository<Message, Long> {
-   List<Message> findByTag(String tag);
+   Page<Message> findAll(Pageable pageable);
+   Page<Message> findByTag(String tag, Pageable pageable);
 
 
 }
